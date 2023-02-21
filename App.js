b4267871c8a8f2159de03a7a7f838474929ac3b4
Enaturale <enaturaleb@gmail.com>
2023-02-21 13:15:45 +0100

    Chat app socket io still misbehaving

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { io } from 'socket.io-client';
 
 export default class App extends Component {
-  
-  componentDidMount(){
-    const socket = io("http://192.168.79.90:3000"); //local IP for the socket connection
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatMessage: "",
+      chatMessages: []
+    }
+  }
+
+  componentDidMount() {
+    this.socket = io("http://192.168.79.90:3000"); //local IP for the socket connection
+    this.socket.on("Chat Message", msg => {
+      this.setState({ chatMessages: [...this.state.chatMessages, msg] })
+    })
 
   }
 
+  submitChatMessage() {
+    this.socket.emit("Chat Message", this.state.chatMessage)
+    this.setState({ chatMessage: "" })
+  }
+
   render() {
+    const chatMessages = this.state.chatMessages.map(
+      chatMessage =>
+        <Text key={chatMessage}>{chatMessage}</Text>
+    )
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+          style={{ height: 40, borderWidth: 2, }}
+          autoCorrect={false}
+          value={this.state.chatMessage}
+          onSubmitEditing={() => this.submitChatMessage()}
+          onChangeText={chatMessage => {
+            this.setState({ chatMessage })
+          }}
+        />
+        {chatMessages}
       </View>
     );
 
@@ -24,7 +54,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
