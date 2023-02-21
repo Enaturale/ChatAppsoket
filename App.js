28bb8310b68bf5b3a73a20300fd460610d74bcb7
Enaturale <enaturaleb@gmail.com>
2023-02-21 12:35:53 +0100

    Socket io client connection done

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { io } from 'socket.io-client';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  
+  componentDidMount(){
+    const socket = io("http://192.168.79.90:3000"); //local IP for the socket connection
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+
+  }
+
 }
 
 const styles = StyleSheet.create({
