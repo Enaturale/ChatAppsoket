ef9d80f24f025094f68a4391e8bfffaeb3b5b83b
Enaturale <enaturaleb@gmail.com>
2023-02-21 11:34:25 +0100

    Installed Socket io and Client

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { io } from 'socket.io-client';
 
 export default function App() {
   return (
